c49aa453c532470336215351b9f593d9ea50e5b7
jalal-sagaie <jalal.sagaie@gmail.com>
2024-11-06 10:18:46 +0100

    saving

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
+import { NoAuthGuard } from '@libs/auth';
+
+
 
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    canActivate:[NoAuthGuard],
+    canActivateChild:[NoAuthGuard],
+    loadChildren: () => import('identity/Routes').then((m) => m.remoteRoutes),
+  },
   {
     path: '',
     component: NxWelcomeComponent,
